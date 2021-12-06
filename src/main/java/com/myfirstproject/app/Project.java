fa141acf7696fdd3e67c2b33a19853d49c7cb472
Anna <annavesel222@gmail.com>
2021-12-06 16:40:44 +0300

    add second hello

--- src/main/java/com/myfirstproject/app/Project.java
+++ src/main/java/com/myfirstproject/app/Project.java
@@ -3,6 +3,7 @@ package com.myfirstproject.app;
 public class Project {
     public static void main(String[] args){
         System.out.println("Hello");
+        System.out.println("Hello");
     }
 }
 
