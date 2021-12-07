a28ea0b310db64b78e2e6f6fc7d9a924ace10591
Anna <annavesel222@gmail.com>
2021-12-07 12:28:37 +0300

    hi

--- src/main/java/com/myfirstproject/app/Project.java
+++ src/main/java/com/myfirstproject/app/Project.java
@@ -3,10 +3,6 @@ package com.myfirstproject.app;
 public class Project {
     public static void main(String[] args){
         System.out.println("Hi");
-        System.out.println("Hi");
-        System.out.println("Hi");
-        System.out.println("Hi");
-        System.out.println("Hi");
 
     }
 }
