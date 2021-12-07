3643e360fd93ad8576420a4f6be1b95c394335a2
Anna <annavesel222@gmail.com>
2021-12-07 12:17:47 +0300

    multi see you later

--- src/main/java/com/myfirstproject/app/Project.java
+++ src/main/java/com/myfirstproject/app/Project.java
@@ -2,12 +2,13 @@ package com.myfirstproject.app;
 
 public class Project {
     public static void main(String[] args){
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
+        System.out.println("See you later");
+        System.out.println("See you later");
+        System.out.println("See you later");
+        System.out.println("See you later");
+        System.out.println("See you later");
+        System.out.println("See you later");
+        System.out.println("See you later");
     }
 }
 
