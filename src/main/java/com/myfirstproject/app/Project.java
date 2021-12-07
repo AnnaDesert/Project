68e7ab177b55c6babd960b25a436cfae46457048
Anna <annavesel222@gmail.com>
2021-12-07 11:54:00 +0300

    hello -> goodbay

--- src/main/java/com/myfirstproject/app/Project.java
+++ src/main/java/com/myfirstproject/app/Project.java
@@ -2,7 +2,7 @@ package com.myfirstproject.app;
 
 public class Project {
     public static void main(String[] args){
-        System.out.println("Hello");
+        System.out.println("Goodbay");
     }
 }
 
